683aef2c22e5aff20e367e741645232f0beecaa2
Eric Evenchick <eric@evenchick.com>
2014-10-23 00:30:36 -0400

    pcan working

--- canard/hw/peak.py
+++ canard/hw/peak.py
@@ -1,34 +1,50 @@
 import os
+import struct
+import socket
 from .. import can
 
 class Pcan:
-    def __init__(self, minor_number):
+    def __init__(self, minor_number=32, ndev="can0"):
         self.device_filename = "/dev/pcan%d" % minor_number
+        self.socket = socket.socket(socket.PF_CAN, socket.SOCK_RAW,
+                                    socket.CAN_RAW)
+        self.ndev = ndev
 
-    def _write_to_device(self, string):
+    def start(self):
+        self.socket.bind((self.ndev,))
+
+    def _write_to_chardev(self, string):
         os.system("echo '%s' > %s" % (string, self.device_filename))
 
     def set_btr(self, value):
-        self._write_to_device('i 0x%X e' % value)
+        self._write_to_chardev('i 0x%X e' % value)
 
-    def write_frame(self, frame):
-        # message string
-        s = "m "
+    def read(self):
+        frame_format = "=IB3xBBBBBBBB"
+        frame_size = struct.calcsize(frame_format)
 
-        # standard or extended frame
-        if frame.is_extended_id:
-            s = s + "e "
-        else:
-            s = s + "s "
+        frame_raw, addr = self.socket.recvfrom(frame_size)
+        id, dlc, d0, d1, d2, d3, d4, d5, d6, d7 = struct.unpack(frame_format,
+                                                                frame_raw)
 
-        # frame identifier
-        s = s + "0x%X " % frame.id
+        frame = can.Frame(id)
+        frame.dlc = dlc
+        frame.data = [d0,d1,d2,d3,d4,d5,d6,d7]
+        return frame
 
-        # length of frame (dlc)
-        s = s + "%d " % frame.dlc
+    def write(self, frame):
+        frame_format = "=IB3xBBBBBBBB"
 
-        # data
-        for i in range(0, frame.dlc):
-            s = s + "0x%X " % frame.data[i]
+        # convert None values into zeros
+        data = []
+        for b in frame.data:
+            if b == None:
+                data.append(0)
+            else:
+                data.append(b)
 
-        self._write_to_device(s)
+        packet = struct.pack(frame_format, frame.id, frame.dlc, data[0],
+                    data[1], data[2], data[3], data[4], data[5], data[6], 
+                    data[7])
+        self.socket.sendto(packet, self.ndev)
+
--- hwtest.py
+++ hwtest.py
@@ -1,13 +1,17 @@
 from canard import can
 from canard.hw import peak
 
-d = peak.Pcan(32)
+d = peak.Pcan()
 
+# set device to 125 kHz
 d.set_btr(0x031c)
 
 f = can.Frame(0x100)
+f.dlc=4
+f.data=[1,2,3,4]
 
-d.write_frame(f)
+print('reading')
+print(d.read())
+print('writing')
+d.write(f)
 
-f = open(d.device_filename, 'r')
-f.readline()
