35c73243f7e6c6595a36d75f1b78926f2ada498f
Eric Evenchick <eric@evenchick.com>
2014-10-24 15:20:38 -0700

    fixed error when receving unspecified message

--- examples/decode.py
+++ examples/decode.py
@@ -11,12 +11,6 @@ dev.start()
 while True:
     frame = dev.recv()
     signals = b.parse_frame(frame)
-    for s in signals:
-        print(s)
-
-f = can.Frame(0x100)
-f.dlc = 8
-f.data = [1, 2, 0x81, 4, 5]
-
-for s in b.parse_frame(f):
-    print(s)
+    if signals:
+        for s in signals:
+            print(s)
