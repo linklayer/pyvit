b95dbe059bd87afb025a5f16be46c48fac7d634d
Eric Evenchick <eric@evenchick.com>
2015-01-27 16:37:57 -0500

    added simple CANtact example

---
+++ examples/cantact.py
@@ -0,0 +1,14 @@
+from canard import can
+from canard.hw import cantact
+
+dev = cantact.CantactDev("/dev/cu.usbmodem14511")
+
+dev.stop()
+dev.ser.write('S0\r')
+dev.start()
+count = 0
+while True:
+    count = count + 1
+    frame = dev.recv()
+    dev.send(frame)
+    print "%d: %s" % (count, str(frame))
