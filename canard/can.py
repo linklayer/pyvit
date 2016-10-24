3e304508bf0a49980b3378d7c7c4993e7b31b909
Eric Evenchick <eric@evenchick.com>
2016-10-24 00:09:53 -0400

    WIP on removing explicit dlc and data padding

--- canard/can.py
+++ canard/can.py
@@ -4,7 +4,6 @@ Defines the low-level implementation of CAN.
 
 """
 
-
 class FrameType:
     """ Enumerates the types of CAN frames """
     DataFrame = 1
@@ -18,19 +17,16 @@ class Frame(object):
 
     Attributes:
         id (int): CAN identifier of the Frame
-        dlc (int): data length code
         data (list of int): CAN data bytes
         frame_type (int): type of CAN frame
-        dlc (int): data length code of frame
         is_extended_id (bool): is this frame an extended identifier frame?
     """
 
-    def __init__(self, id, dlc=0, data=[], frame_type=FrameType.DataFrame,
+    def __init__(self, id, data=[], frame_type=FrameType.DataFrame,
                  is_extended_id=False):
         """ Initializer of Frame
         Args:
             id (int): identifier of CAN frame
-            dlc (int, optional): data length code of frame (defaults to 0)
             data (list, optional): data of CAN frame, defaults to empty list
             frame_type (int, optional): type of frame, defaults to
                                         FrameType.DataFrame
@@ -38,10 +34,7 @@ class Frame(object):
                                              defaults to False
         """
 
-        # these 2 fields must be defined before 'id' and'data', otherwise
-        # 'no attribute ' exception will be raised
         self.is_extended_id = is_extended_id
-        self.dlc = dlc
 
         self.id = id
         self.data = data
@@ -67,11 +60,7 @@ class Frame(object):
 
     @property
     def data(self):
-        # return bytes up to dlc length, pad with zeros
-        data_len = min(self.dlc, len(self._data))
-        result = self._data[:data_len]
-        result.extend([0] * (8 - data_len))
-        return result
+        return self._data
 
     @data.setter
     def data(self, value):
@@ -99,29 +88,7 @@ class Frame(object):
 
     @property
     def dlc(self):
-        return self._dlc
-
-    @dlc.setter
-    def dlc(self, value):
-        assert isinstance(value, int)
-        assert value >= 0 and value <= 8, 'dlc must be between 0 and 8'
-        self._dlc = value
+        return len(data)
 
     def __str__(self):
-        ext_str = 'standard'
-        if self.is_extended_id:
-            ext_str = 'extended'
-        if self.frame_type == FrameType.DataFrame:
-            type_str = "data"
-        elif self.frame_type == FrameType.RemoteFrame:
-            type_str = "remote"
-        elif self.frame_type == FrameType.ErrorFrame:
-            type_str = "error"
-        elif self.frame_type == FrameType.OverloadFrame:
-            type_str = "overload"
-
-        result = 'Frame:'
-        result = result + '\tID=0x%X, DLC=%d, ' % (self.id, self.dlc)
-        result = result + '%s, %s' % (ext_str, type_str)
-        result = result + '\n\tData=%s' % self.data
-        return result
+        return 'ID=0x%03X, DLC=%d, Data=[%s]' % (self.id, self.dlc, ', '.join(hex(b) for b in self.data))
