6a13e00a35276d85ec0aa5dd1d97388e50377966
ulabs-sgiroux <sag@ulabs.io>
2016-12-13 23:14:16 -0500

    Fixed file_candump_test on windows
    Just grabs a temp file instead of relying on the UNIX filesystem.
    
    Additionally, adds support for factors <1.0 in message database files
    with included tests

---
+++ test/json_db_test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import pyvit.can as can
+from pyvit.file.db.jsondb import JsonDbParser
+
+import unittest
+
+
+class FileDbJsonDbTest(unittest.TestCase):
+    def test_defaults(self):
+        """ Test parsing a json database """
+        uut = JsonDbParser()
+
+        """ Should not throw any exceptions or errors """
+        file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'vector_jsondb.json')
+        bus_db = uut.parse(file_path)
+
+        """ [600 RPM, 4th gear, 2 units of voltage, unit to be divided by 10, unit with offset] """
+        frame_1 = can.Frame(0x123, [88, 2, 0b00010100, 50, 10])
+
+        signals = bus_db.parse_frame(frame_1)
+        """ Need help asserting equality for signal values. """
+        """ The order seems to change (array) """
+
+if __name__ == '__main__':
+    unittest.main()
