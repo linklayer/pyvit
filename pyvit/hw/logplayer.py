5def69afee0c54b86c0dda8ab95adafb9b0cfaa7
Eric Evenchick <eric@evenchick.com>
2017-03-24 12:16:35 -0400

    converted logging to candump format, implemented logging tests
    Signed-off-by: Eric Evenchick <eric@evenchick.com>

--- pyvit/hw/logplayer.py
+++ pyvit/hw/logplayer.py
@@ -5,16 +5,34 @@ from .. import can
 class LogPlayer:
     running = False
 
-    def __init__(self, log_filename):
-        self.logfile = open(log_filename, 'r')
+    def __init__(self, log_filename, realtime=True):
+        self.log_filename = log_filename
+        self.realtime = realtime
 
     def start(self):
         assert not self.running, 'cannot start, already running'
-        self.start_timestamp = time.time()
+
+        self.logfile = open(self.log_filename, 'r')
+        self.start_timestamp = None
         self.running = True
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def stop(self):
+        self.running = False
+        self.logfile.close()
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.stop()
+
+    def send(self, data):
+        pass
 
     def recv(self):
         assert self.running, 'not running'
+
         line = self.logfile.readline()
         if line == '':
             return None
@@ -22,34 +40,40 @@ class LogPlayer:
         # convert line to frame
         frame = self._log_to_frame(line)
 
+        # make the first frame's timestamp now
+        if self.start_timestamp is None:
+            self.start_timestamp = time.time() - frame.timestamp
         # sleep until message occurs
-        time.sleep(max((self.start_timestamp - time.time() + frame.timestamp),
-                   0))
+        if self.realtime:
+            time.sleep(max((self.start_timestamp - time.time() +
+                            frame.timestamp), 0))
 
         return frame
+
+    def recv_all(self):
+        frames = []
+
+        line = self.logfile.readline()
+        while line != '':
+            frame = self._log_to_frame(line)
+            frames.append(frame)
+            line = self.logfile.readline()
+
+        return frames
 
     def _log_to_frame(self, line):
         fields = line.split(' ')
 
-        arb_id = int(fields[1], 0)
+        arb_id = int(fields[2].split('#')[0], 16)
         frame = can.Frame(arb_id)
 
-        frame.timestamp = float(fields[0])
+        frame.timestamp = float(fields[0][1:-1])
 
-        if fields[2].upper() == 'D':
-            frame.frame_type = can.FrameType.DataFrame
-        elif fields[2].upper() == 'R':
-            frame.frame_type = can.FrameType.RemoteFrame
-        elif fields[2].upper() == 'E':
-            frame.frame_type = can.FrameType.ErrorFrame
-        elif fields[2].upper() == 'O':
-            frame.frame_type = can.FrameType.OverloadFrame
-        else:
-            raise ValueError('invalid frame type')
+        datastr = fields[2].split('#')[1]
+        dlc = int(len(datastr) / 2)
 
-        dlc = int(fields[3])
         frame.data = []
         for i in range(0, dlc):
-            frame.data.append(int(fields[i+4], 0))
+            frame.data.append(int(datastr[i*2:i*2+2], 16))
 
         return frame
--- pyvit/log.py
+++ pyvit/log.py
@@ -1,27 +1,41 @@
 from . import can
+import time
 
 
 class Logger:
-    def __init__(self):
-        self._buffer = []
+    def __init__(self, filename, if_name='can0'):
+        self.if_name = if_name
+        self.filename = filename
+        self.started = False
 
-    def log(self, frame):
+    def start(self):
+        self.start_timestamp = time.time()
+        self.started = True
+        self._file = open(self.filename, 'w')
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def stop(self):
+        self.started = False
+        self._file.close()
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        self.stop()
+
+    def log_frame(self, frame):
         assert isinstance(frame, can.Frame), 'invalid frame'
 
-        if frame.frame_type == can.FrameType.DataFrame:
-            type_char = "D"
-        elif frame.frame_type == can.FrameType.RemoteFrame:
-            type_char = "R"
-        elif frame.frame_type == can.FrameType.ErrorFrame:
-            type_char = "E"
-        elif frame.frame_type == can.FrameType.OverloadFrame:
-            type_char = "O"
+        if not self.started:
+            raise Exception('logger not started')
 
-        line = ("%f 0x%X %s %d 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X\n" %
-                (frame.timestamp,
+        ts = time.time() - self.start_timestamp
+
+        line = ("(%f) %s %03X#%02X%02X%02X%02X%02X%02X%02X%02X\n" %
+                (ts,
+                 self.if_name,
                  frame.arb_id,
-                 type_char,
-                 frame.dlc,
                  frame.data[0],
                  frame.data[1],
                  frame.data[2],
@@ -30,17 +44,7 @@ class Logger:
                  frame.data[5],
                  frame.data[6],
                  frame.data[7]))
-        self._buffer.append(line)
-
-    def __str__(self):
-        s = ""
-        for line in self._buffer:
-            s = s + line
-        return s
-
-    def write_file(self, filename):
-        with open(filename, 'w') as logfile:
-            logfile.writelines(self._buffer)
+        self._file.write(line)
 
     def clear(self):
         self._buffer = []
